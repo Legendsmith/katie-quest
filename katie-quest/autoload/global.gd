0531f2ba1326f1847b228a02ca023d82f307979d
Legendsmith <legendsmith.au@gmail.com>
2025-08-03 01:27:07 +1000

    Texture and lights

--- katie-quest/autoload/global.gd
+++ katie-quest/autoload/global.gd
@@ -1,4 +1,5 @@
 extends Node
+signal light_level(energy:float)
 const TILE_SIZE:Vector2i = Vector2i(64,64)
 
 var view_label_string:String = ""
@@ -7,3 +8,6 @@ var view_label_string:String = ""
 func grid_snap(tile_layer, node:Node2D):
 	var snapped_pos:Vector2 = tile_layer.to_global(tile_layer.map_to_local(tile_layer.local_to_map(tile_layer.to_local(node.global_position))))
 	return snapped_pos
+
+func set_light_level(new_energy:float):
+	light_level.emit(new_energy)
