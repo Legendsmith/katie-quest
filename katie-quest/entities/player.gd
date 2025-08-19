b51a7b657fefa5b4a1112a079ce78153453594db
Legendsmith <legendsmith.au@gmail.com>
2025-08-20 03:14:24 +1000

    Revert "WIP Feature: Fufu level"

--- katie-quest/entities/player.gd
+++ katie-quest/entities/player.gd
@@ -1,68 +1,33 @@
 class_name Player
 extends CharacterBody2D
 
-@export var move_speed:float = 5000.0
+@export var move_speed:float = 8000.0
+@export var sprint_bonus:float = 1.0
 @export var items:Inventory
 
-@export var max_speed: float = 10000.0
-@export var acceleration: float = 3000.0
-@export var deceleration: float = 5000.0
-@export var bounce_force: float = 1000.0
-
 @onready var audio_player:AudioStreamPlayer = $AudioStreamPlayer
-var move_target:Vector2
-var facing:Vector2 = Vector2.RIGHT
-var moving = false
 
-func _ready():
-	add_to_group("player")
+var move_target:Vector2
+var moving = false
 
 func play_sound(sound:AudioStream):
 	audio_player.stream=sound
 	audio_player.play()
 
 func _physics_process(delta):
-	if Input.is_action_pressed(&"sprint"):
-		var target_velocity = global_position.direction_to(move_target) * max_speed
-		velocity = velocity.move_toward(target_velocity, acceleration * delta)
-	elif moving:
-		var speed = move_speed
+	if moving:
+		var speed = move_speed * (1 + (sprint_bonus * Input.get_action_strength(&"sprint")))
 		velocity = global_position.direction_to(move_target) * speed * delta
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
-	move_and_slide()
-	handle_wall_bouncing()
-
+		move_and_slide()
 
 func _process(_delta):
 	movement()
 
 
 func movement():
-	var input_dir:Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
-	if input_dir.is_zero_approx(): # early exit
+	var new_input:Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
+	if new_input.is_zero_approx(): # early exit
 		moving = false
 		return
-	var angle = input_dir.angle() / (PI/2)
-	angle = wrapi(int(angle), 0, 4)
-	$AnimationPlayer.play("look_"+str(angle))
-	move_target = global_position + (input_dir * Vector2(Global.TILE_SIZE))
+	move_target = global_position + (new_input * Vector2(Global.TILE_SIZE))
 	moving = true
-
-func handle_wall_bouncing():
-	# Check if we collided with something
-	if get_slide_collision_count() > 0:
-		for i in get_slide_collision_count():
-			var collision = get_slide_collision(i)
-			var collision_normal = collision.get_normal()
-			
-			# Calculate bounce velocity
-			# Reflect the velocity off the surface normal
-			var bounce_velocity = velocity.bounce(collision_normal)
-			
-			# Apply bounce force (optional: scale it down for more realistic bouncing)
-			velocity = bounce_velocity * 0.4  # 60% of original velocity maintained
-			
-			# Optional: Add a minimum bounce force for more dramatic effect
-			if velocity.length() < bounce_force:
-				velocity = collision_normal * bounce_force
--- katie-quest/scripts/interactable.gd
+++ katie-quest/scripts/interactable.gd
@@ -3,7 +3,7 @@ extends Area2D
 signal interacted(player_actor:Node2D)
 const VIEW_LABEL_TEXT_FUNCTION = "get_view_label_text"
 @export var view_label_default:String = ""
-@export var auto_interact:bool = false
+@export var auto_interact:bool = true
 @export var cursor_shape:Input.CursorShape = Input.CursorShape.CURSOR_POINTING_HAND
 var _get_view_label_text:Callable = func(): return view_label_default
 ## Used if the parent or owner do not have view label functionality.
@@ -15,12 +15,9 @@ func _init():
 	collision_layer = 2
 	collision_mask = 0
 	if auto_interact: # for things like keys that automatically pickup.
-		enable_auto_interact()
-
-func enable_auto_interact():
-	monitoring=true
-	collision_mask = 1
-	body_entered.connect(on_interaction)
+		monitoring=true
+		collision_mask = 1
+		body_entered.connect(on_interaction)
 
 func _ready():
 	if get_parent().has_method(VIEW_LABEL_TEXT_FUNCTION):
@@ -33,6 +30,6 @@ func get_view_label_text():
 	return _get_view_label_text.call()
 
 func on_interaction(actor:Node2D) -> void:
-	#print_debug(actor.name, " interacted with ", name, "!")
+	print_debug(actor.name, " interacted with ", name, "!")
 	interacted.emit(actor)
 	
