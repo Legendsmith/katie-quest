c1df7f3fd727f380f60404f4e5b6fc66bfa545d6
Legendsmith <legendsmith.au@gmail.com>
2025-08-22 00:05:10 +1000

    Update character_navigation.gd

--- katie-quest/entities/character_navigation.gd
+++ katie-quest/entities/character_navigation.gd
@@ -2,8 +2,15 @@ extends RigidBody2D
 
 @export var target: Node2D
 @export var speed: float = 200.0
+@export var desired_range: float = 150.0
 
 @onready var nav_agent = $NavigationAgent2D
+
+func _ready():
+	VerySimpleTwitch.chat_message_received.connect(chat_talk)
+
+func chat_talk(chatter:VSTChatter):
+	speech_bubble.set_text(chatter.message)
 
 func _physics_process(_delta):
   if target == null:
