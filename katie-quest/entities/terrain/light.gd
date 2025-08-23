e143e60b14e32642127de63958771ace860f7cfe
Legendsmith <legendsmith.au@gmail.com>
2025-08-24 07:34:36 +1000

    Reset lighting settings

--- katie-quest/entities/terrain/light.gd
+++ katie-quest/entities/terrain/light.gd
@@ -2,5 +2,9 @@ extends PointLight2D
 func _ready():
 	Global.light_level.connect(_on_light_level)
 
-func _on_light_level(new_energy:float):
+func _on_light_level(new_energy:float, fade_time=0.0):
+	if fade_time:
+		var tween = Tween.new()
+		tween.tween_property(self,^"energy",new_energy,fade_time)
+		return
 	energy=new_energy
